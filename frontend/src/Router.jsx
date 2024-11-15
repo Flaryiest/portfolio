38342a166e2d78b8a3507d95426ec724003389ad
Flaryiest <ericmzuo1@gmail.com>
2024-11-15 16:03:59 -0700

    set up file structure + base css

---
+++ frontend/src/Router.jsx
@@ -0,0 +1,10 @@
+import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
+const Router = createBrowserRouter(
+    createRoutesFromElements(
+        <>
+        
+        </>
+    )
+)
+
+export default Router
