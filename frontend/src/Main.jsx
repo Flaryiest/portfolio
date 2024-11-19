2eaa0cf1f7a90f5d1f4cc4d08cd82bf07eb9dd76
Flaryiest <ericmzuo1@gmail.com>
2024-11-18 18:52:47 -0700

    Update Main.jsx

--- frontend/src/Main.jsx
+++ frontend/src/Main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './style/Index.css'
-import Router from './router.jsx'
+import Router from './router'
 
 
 createRoot(document.getElementById('root')).render(
