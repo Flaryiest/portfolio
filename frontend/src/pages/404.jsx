ef05f03d28d0b519391adcee838bb05e2390653f
Flaryiest <ericmzuo1@gmail.com>
2024-11-18 19:16:33 -0700

    import change

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -1,11 +1,5 @@
-import Navbar from "../components/navbar"
-import AnimatedCursor from "react-animated-cursor"
 export default function ErrorPage() {
     return <div>
-        <div className="cursor-wrapper">
-            <AnimatedCursor color="68, 98, 74" zIndex={2000} />
-         </div>
-        <Navbar />
         <p className="error-message">This page doesn't exist sadly</p>
     </div>
 }
