da73d83dcf52d39abd2bd44aadab419f2cb3e7b0
Flaryiest <ericmzuo1@gmail.com>
2024-11-20 18:07:00 -0700

    created contact page elements

--- frontend/src/pages/contact.jsx
+++ frontend/src/pages/contact.jsx
@@ -1,8 +1,48 @@
-export default function ContactPage() {
-    return <div className="contact-page">
-        <form className="contact-form">
-        
-        </form>
+import React, { useState } from 'react';
+import '../style/contact.css';
+const ContactPage = () => {
+  const [message, setMessage] = useState('')
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Form Submitted:", { message });
+    setMessage('');
+    await sendMessage()
+  }
+  
+  async function sendMessage() {
+    const response = await fetch("https://swag.up.railway.app/"+ "feedback/submit", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify({message: message}),
+  })
+  const data = await response.json()
+  if (data) {
+      console.log(data)
+  }   
+  }
 
+  return (
+    <div className="contact-form-container">
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <h2 className="contact-form-title">Contact</h2>
+
+        <label htmlFor="message" className="contact-form-label">Feedback</label>
+        <textarea
+          id="message"
+          className="contact-form-textarea"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Write your message here..."
+          required
+        />
+
+        <button type="submit" className="contact-form-button">Submit</button>
+      </form>
     </div>
+  )
 }
+
+export default ContactPage
--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -3,14 +3,19 @@ import {
     createBrowserRouter,
     createRoutesFromElements
 } from 'react-router-dom';
+import Layout from './layout.jsx';
+
 import IndexPage from './pages/index.jsx';
 import ErrorPage from './pages/404.jsx';
-import Layout from './layout.jsx';
+import ContactPage from './pages/contact.jsx';
+
+
 
 const Router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<Layout />}>
             <Route index element={<IndexPage />} />
+            <Route path="/contact" element={<ContactPage/>} />
             <Route path="*" element={<ErrorPage />} />
         </Route>
     )
